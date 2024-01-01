5d531f28a4d5a2a7557df6b4904e2e64198e0e0f
Sarkis-c1 <146673852+Sarkis-c1@users.noreply.github.com>
2023-12-31 17:06:55 -0800

    Redefine file

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-module.exports = Phrase
+module.exports = Phrase;
+
+String.prototype.reverse = function reverse(string) {
+    return Array.from(this).reverse().join("");
+}
+
+
 
 function reverse(string) {
     return string.split("").reverse().join("");
 }
 
-function palindrome(string) {
-    let processedContent = string.toLowerCase();
-    return processedContent === reverse(processedContent);
+// Defines a Phrase object.
+function Phrase(content) {
+    this.content = content
 
+
+// Returns content processed for palindrome testing.
+this.processedContent = function processedContent() {
+    return this.coontent.toLowerCase();
 }
+
+// Returns true if a palindrome, false otherwise
+this.palindrome = function palindrome() {
+        return this.processedContent === this.processedContent().reverse;
+}
+}
+
+// Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, tranlation) {
+    this.content = content;
+    this.translation = translation;
+
+// Returns content processed for palindrome testing.
+this.processedContent = function processedContent() {
+    return this.tranlation.toLowerCase();
+}}
+
+String.prototype.palindroe= function palindrome() {
+    return new Phrase(this).palindrome();
+}
+
